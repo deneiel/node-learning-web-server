3ef406b3e92fc870e911f024c35ed683cde1b8a0
Daniel Jimenez Martinez <deneiel@pajaroslolos.com>
2021-05-22 23:31:11 -0500

    Setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,3 @@
-fetch("http://puzzle.mead.io/puzzle").then((response) => {
-  response.json().then((data) => {
-    console.log(data);
-  });
-});
-
 const weatherForm = document.querySelector("form");
 const formInput = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
@@ -17,19 +11,17 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "loading...";
   messageTwo.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          messageTwo.textContent = "";
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = `${data.weather_descriptions.join(
-            ", "
-          )}. It is ${data.temperature} and feels like ${data.feelslike}.`;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = "";
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = `${data.weather_descriptions.join(
+          ", "
+        )}. It is ${data.temperature} and feels like ${data.feelslike}.`;
+      }
+    });
+  });
 });
